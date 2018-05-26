171d9946e8f4ea398b72833fea6392a5f2576146
Chris Berthold <cberthold@baxtertechnology.com>
2018-05-26 09:00:08 -0400

    start adding unit tests for projects

---
+++ CqrsDsl.Tests/AbstractParserTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Antlr4.Runtime;
+using CqrsDsl.Generated;
+
+namespace CqrsDsl.Tests
+{
+    public abstract class AbstractParserTest
+    {
+        protected ITokenStream GetTokenStream(string input)
+        {
+            var inputStream = new AntlrInputStream(input);
+            var lexer = new CqrsLexer(inputStream);
+            var tokenStream = new CommonTokenStream(lexer);
+            return tokenStream;
+        }
+    }
+}
---
+++ CqrsDsl.Tests/ProjectAssignments/Given_project_line.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CqrsDsl.Generated;
+using Xunit;
+
+namespace CqrsDsl.Tests.ProjectAssignments
+{
+    public class Given_project_line : AbstractParserTest
+    {
+        public const string PROJECT_NAME = "TestProject";
+        public const string PROJECT_NEW_LINE = "project TestProject\r\n";
+        public const string PROJECT_NO_NEW_LINE = "project TestProject";
+
+        [Theory]
+        [InlineData(PROJECT_NEW_LINE)]
+        [InlineData(PROJECT_NO_NEW_LINE)]
+        public void Should_Parse_Without_Exception(string input)
+        {
+            var tokenStream = GetTokenStream(input);
+            var parser = new CqrsParser(tokenStream);
+            var dictionary = parser.dictionary();
+        }
+
+        [Theory]
+        [InlineData(PROJECT_NEW_LINE)]
+        [InlineData(PROJECT_NO_NEW_LINE)]
+        public void Should_return_one_project_assignment(string input)
+        {
+            var tokenStream = GetTokenStream(input);
+            var parser = new CqrsParser(tokenStream);
+            var dictionary = parser.dictionary();
+            Assert.Single(dictionary.projectAssignments());
+            Assert.Single(dictionary.projectAssignments(0).projectAssignment());
+        }
+
+
+        [Theory]
+        [InlineData(PROJECT_NEW_LINE)]
+        [InlineData(PROJECT_NO_NEW_LINE)]
+        public void Should_return_test_project_name(string input)
+        {
+            var tokenStream = GetTokenStream(input);
+            var parser = new CqrsParser(tokenStream);
+            var dictionary = parser.dictionary();
+            var assignment = dictionary.projectAssignments(0).projectAssignment(0);
+            Assert.Equal(PROJECT_NAME, assignment.projectName().GetText());
+        }
+    }
+}
