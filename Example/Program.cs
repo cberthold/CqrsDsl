26617e510b6ef42404fb96283f2f3982ea723e26
Chris Berthold <cberthold@baxtertechnology.com>
2018-05-25 22:59:47 -0400

    add an example program and test document to try out the dsl works

---
+++ Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Antlr4.Runtime;
+using CqrsDsl;
+using CqrsDsl.Generated;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = File.ReadAllText("Example.cqrsx");
+            AntlrInputStream inputStream = new AntlrInputStream(file);
+            CqrsLexer lexer = new CqrsLexer(inputStream);
+            CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+            CqrsParser parser = new CqrsParser(commonTokenStream);
+            
+            var vistor = new DebugVisitor();
+            var dictionary = parser.dictionary();
+            vistor.VisitDictionary(dictionary);
+            Console.ReadKey();
+        }
+    }
+}
