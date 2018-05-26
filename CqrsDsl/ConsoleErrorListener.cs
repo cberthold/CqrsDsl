fd4b656b986669661ae7efd2e72cc97c22d47f2e
Chris Berthold <cberthold@baxtertechnology.com>
2018-05-26 07:59:49 -0400

    add console error listener for debugging parsing as it goes

---
+++ CqrsDsl/ConsoleErrorListener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Antlr4.Runtime;
+
+namespace CqrsDsl
+{
+    public class ConsoleErrorListener<Symbol> : IAntlrErrorListener<Symbol>
+    {
+        public void SyntaxError(IRecognizer recognizer, Symbol offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+        {
+            string formattedError = $"Error at line: {line}:{charPositionInLine} - {offendingSymbol} - {msg}";
+            Console.Error.WriteLine(formattedError);
+        }
+    }
+}
