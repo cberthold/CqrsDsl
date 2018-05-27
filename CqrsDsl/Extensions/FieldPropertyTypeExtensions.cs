1abd6b2f8045e39a6339939e064b9fe9c2723286
Chris Berthold <cberthold@baxtertechnology.com>
2018-05-27 11:16:45 -0400

    add extensions for getting modifiers from field properties

---
+++ CqrsDsl/Extensions/FieldPropertyTypeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static CqrsDsl.Generated.CqrsParser;
+
+namespace CqrsDsl
+{
+    public static class FieldPropertyTypeExtensions
+    {
+        public static bool IsPredefinedType(this FieldPropertyTypeContext context)
+        {
+            return context.GetTokens(PREDEFINED_TYPE).Length == 1;
+        }
+
+        public static bool IsOptional(this FieldPropertyTypeContext context)
+        {
+            return context.GetTokens(OPTIONAL).Length == 1;
+        }
+
+        public static bool IsRequired(this FieldPropertyTypeContext context)
+        {
+            return context.GetTokens(REQUIRED).Length == 1;
+        }
+
+    }
+}
