fbf743c35797a4bfe845fd7e2632be534b5be982
Chris Berthold <cberthold@baxtertechnology.com>
2018-05-27 08:42:20 -0400

    add in testing project line for handling periods and underscores in the language

--- CqrsDsl.Tests/ProjectAssignments/Given_project_line.cs
+++ CqrsDsl.Tests/ProjectAssignments/Given_project_line.cs
@@ -9,13 +9,20 @@ namespace CqrsDsl.Tests.ProjectAssignments
     public class Given_project_line : AbstractParserTest
     {
         public const string PROJECT_NAME = "TestProject";
+        public const string PERIOD_PROJECT_NAME = "TestProject.Period";
+        public const string UNDERSCORE_PROJECT_NAME = "TestProject_Underscore";
+
         public const string PROJECT_NEW_LINE = "project TestProject\r\n";
         public const string PROJECT_NO_NEW_LINE = "project TestProject";
+        public const string PROJECT_PERIOD = "project TestProject.Period";
+        public const string PROJECT_UNDERSCORE = "project TestProject_Underscore";
 
         [Theory]
         [InlineData(PROJECT_NEW_LINE)]
         [InlineData(PROJECT_NO_NEW_LINE)]
-        public void Should_Parse_Without_Exception(string input)
+        [InlineData(PROJECT_PERIOD)]
+        [InlineData(PROJECT_UNDERSCORE)]
+        public void Should_parse_without_exception(string input)
         {
             var tokenStream = GetTokenStream(input);
             var parser = new CqrsParser(tokenStream);
@@ -25,13 +32,14 @@ namespace CqrsDsl.Tests.ProjectAssignments
         [Theory]
         [InlineData(PROJECT_NEW_LINE)]
         [InlineData(PROJECT_NO_NEW_LINE)]
+        [InlineData(PROJECT_PERIOD)]
+        [InlineData(PROJECT_UNDERSCORE)]
         public void Should_return_one_project_assignment(string input)
         {
             var tokenStream = GetTokenStream(input);
             var parser = new CqrsParser(tokenStream);
             var dictionary = parser.dictionary();
-            Assert.Single(dictionary.projectAssignments());
-            Assert.Single(dictionary.projectAssignments(0).projectAssignment());
+            Assert.Single(dictionary.GetAllProjectAssignments());
         }
 
 
@@ -43,8 +51,29 @@ namespace CqrsDsl.Tests.ProjectAssignments
             var tokenStream = GetTokenStream(input);
             var parser = new CqrsParser(tokenStream);
             var dictionary = parser.dictionary();
-            var assignment = dictionary.projectAssignments(0).projectAssignment(0);
-            Assert.Equal(PROJECT_NAME, assignment.projectName().GetText());
+            var assignment = dictionary.GetProjectAssignmentItem(0);
+            Assert.Equal(PROJECT_NAME, assignment.GetProjectNameText());
+        }
+
+
+        [Fact]
+        public void Should_return_full_project_name_with_period()
+        {
+            var tokenStream = GetTokenStream(PROJECT_PERIOD);
+            var parser = new CqrsParser(tokenStream);
+            var dictionary = parser.dictionary();
+            var assignment = dictionary.GetProjectAssignmentItem(0);
+            Assert.Equal(PERIOD_PROJECT_NAME, assignment.GetProjectNameText());
+        }
+
+        [Fact]
+        public void Should_return_full_project_name_with_underscore()
+        {
+            var tokenStream = GetTokenStream(PROJECT_UNDERSCORE);
+            var parser = new CqrsParser(tokenStream);
+            var dictionary = parser.dictionary();
+            var assignment = dictionary.GetProjectAssignmentItem(0);
+            Assert.Equal(UNDERSCORE_PROJECT_NAME, assignment.GetProjectNameText());
         }
     }
 }
